702a6a4fcc83d213b426152a2fd843c3901c6421
poJLikno <pojlikno@gmail.com>
2022-11-28 22:23:28 +0300

    Create main.cpp

---
+++ RayCastingV2/main.cpp
@@ -0,0 +1,16 @@
+#include "headers/main.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prev, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    try
+    {
+        GameWindow game_window("Game", 100, 100, hInstance);
+        game_window.ParseGameWindowMessages(game_window.msg);
+    }
+    catch (runtime_error error)
+    {
+        MessageBox(NULL, error.what(), "Error", MB_OK);
+    }
+
+    return 0;
+}
